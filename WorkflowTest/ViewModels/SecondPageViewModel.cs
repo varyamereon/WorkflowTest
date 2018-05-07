c809078f89a2c78de5bdef8d502332e0bc7352a6
Will Beal <willbeal@outlook.com>
2018-05-07 12:05:56 +0200

    Added artists collection and listview.

--- WorkflowTest/ViewModels/SecondPageViewModel.cs
+++ WorkflowTest/ViewModels/SecondPageViewModel.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.ObjectModel;
 using Prism.Windows.Mvvm;
 
 namespace WorkflowTest.ViewModels
@@ -8,6 +8,21 @@ namespace WorkflowTest.ViewModels
     {
         public SecondPageViewModel()
         {
+            artists = new ObservableCollection<string>()
+            {
+                "Bruce Springsteen",
+                "Pink Floyd",
+                "Josh Ritter",
+                "Aimee Mann",
+                "Jonathon Coulton"
+            };
+        }
+
+        private ObservableCollection<string> artists;
+        public ObservableCollection<string> Artists
+        {
+            get => artists;
+            set => SetProperty(ref artists, value);
         }
     }
 }
